2ba4218e37264fd46d43a7d27fc8eacb912ae32a
Karla C. Ferreira <karlinharural@gmail.com>
2025-07-11 19:30:58 +0000

    create-structure

--- backend/api/main.py
+++ backend/api/main.py
@@ -19,7 +19,6 @@ app.add_middleware(
 def get_clima():
     """
     Fetches the current weather data for a specific location.
-    
     lat = "-21.76"
     lon = "-43.34"
     key = os.getenv("WEATHER_API_KEY")
